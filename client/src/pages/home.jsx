c0ebd720ac68172837ee09aadec1d555b07375e9
Teo Mesrkhani <teomesrkhani@gmail.com>
2025-05-03 11:45:23 -0400

    Remove unused title component

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,16 +1,12 @@
 import '../styles/home.css';
-import { Route, Routes, Router } from "react-router-dom";
-import React, { useState } from 'react';
 
 // Components
-import Title from '../components/Title';
 import Content from '../components/Content'
 
 function Home() {
   return (
     <div className="recommend-painting">
       <Content/>
-      <Title currentPage="recommend-painting"/>
     </div>
   );
 }
